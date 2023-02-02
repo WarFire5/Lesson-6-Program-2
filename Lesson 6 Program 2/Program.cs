fed9c4dc5c1660e781f1e3afa2c888b0cde029b1
Татьяна <@WIN-E362SIULNK8>
2023-02-03 01:34:57 +0300

    Добавьте файлы проекта.

---
+++ Lesson 6 Program 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6_Program_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string A, B, BA;
+
+            Console.WriteLine("Задайте число 'A':");
+            A = Console.ReadLine();
+
+            Console.WriteLine("Задайте число 'B':");
+            B = Console.ReadLine();
+
+            int a = Convert.ToInt32(A);
+
+            int b = Convert.ToInt32(B);
+
+            int temp;
+
+            temp = a;
+            a = b;
+            b = temp;
+
+            BA = a + " " + b + ".";
+
+            Console.WriteLine(BA);
+
+            Console.ReadLine();
+
+        }
+    }
+}
